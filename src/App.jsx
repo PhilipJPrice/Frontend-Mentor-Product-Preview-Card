a18b70deb466e9e76abf2db00b7c88b70e8225c1
PhilipJPrice <philip.price.1994@gmail.com>
2025-09-02 12:18:17 -0700

    fix: changed class to className in JSX elements

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ import productDesktop from './assets/images/image-product-desktop.jpg';
 
 export function App() {
 	return (
-		<div class="flex justify-center items-center min-h-screen">
-			<div class="mx-auto w-84 pb-8 bg-white rounded-xl my-5 md:flex md:rounded-lg md:h-112 md:w-172">
-				<img src={productMobile} alt="Picture of perfume: Gabrielle by Chanel" class="block rounded-t-xl md:hidden mb-6" />	
-				<img src={productDesktop} alt="Picture of perfume: Gabrielle by Chanel" class="hidden md:block rounded-l-lg h-auto" />
-				<div class="px-4 md:px-8 md:pr-12 md:py-8">
-					<h4 class="font-montserrat text-sm text-zinc-400 tracking-[.5em] mb-6">PERFUME</h4>
-					<h2 class="font-fraunces font-black text-4xl px-auto mb-6"><b>Gabrielle Essence Eau De Parfum</b></h2>
-					<p class="font-montserrat text-zinc-400 mb-6">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
-					<div class="flex items-center mb-4 md:mb-6">
-						<h4 class="font-fraunces text-4xl font-black text-emerald-700"><b>$149.99</b></h4>
-						<p class="font-montserrat pl-4 text-zinc-400"><s>$169.99</s></p>
+		<div className="flex justify-center items-center min-h-screen">
+			<div className="mx-auto w-84 pb-8 bg-white rounded-xl my-5 md:flex md:rounded-lg md:h-112 md:w-172">
+				<img src={productMobile} alt="Picture of perfume: Gabrielle by Chanel" className="block rounded-t-xl md:hidden mb-6" />	
+				<img src={productDesktop} alt="Picture of perfume: Gabrielle by Chanel" className="hidden md:block rounded-l-lg h-auto" />
+				<div className="px-4 md:px-8 md:pr-12 md:py-8">
+					<h4 className="font-montserrat text-sm text-zinc-400 tracking-[.5em] mb-6">PERFUME</h4>
+					<h2 className="font-fraunces font-black text-4xl px-auto mb-6"><b>Gabrielle Essence Eau De Parfum</b></h2>
+					<p className="font-montserrat text-zinc-400 mb-6">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+					<div className="flex items-center mb-4 md:mb-6">
+						<h4 className="font-fraunces text-4xl font-black text-emerald-700"><b>$149.99</b></h4>
+						<p className="font-montserrat pl-4 text-zinc-400"><s>$169.99</s></p>
 					</div>
-					<button class="flex items-center text-center justify-center bg-emerald-700 rounded-md w-full h-14 hover:bg-emerald-900 cursor-pointer">
+					<button className="flex items-center text-center justify-center bg-emerald-700 rounded-md w-full h-14 hover:bg-emerald-900 cursor-pointer">
 						<img src={cart} alt="SVG image of shopping cart" />
-						<p class="font-montserrat text-lg text-white pl-2"><b>Add to Cart</b></p>
+						<p className="font-montserrat text-lg text-white pl-2"><b>Add to Cart</b></p>
 					</button>
 				</div>
 			</div>
